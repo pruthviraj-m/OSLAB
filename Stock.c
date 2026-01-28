16178d926f89dc82e067af3f89740d5dcf26389d
Pruthviraj M <pruthviraj130606@gmail.com>
2026-01-29 00:17:16 +0530

    Add files via upload

---
+++ Stock.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+    } else {
+        stack[++top] = value;
+        printf("Pushed: %d\n", value);
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow! Nothing to pop\n");
+    } else {
+        printf("Popped: %d\n", stack[top--]);
+    }
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements (top to bottom): ");
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+    display();
+
+    return 0;
+}
